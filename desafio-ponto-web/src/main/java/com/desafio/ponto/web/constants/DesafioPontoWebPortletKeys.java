22a2efe107513fd7b8bd2253cbf6339f29971f2f
de Morais, Peterson V <peterson.v.de.morais@accenture.com>
2018-12-26 17:15:30 -0300

    Projeto Desafio Ponto Web (Portlet)

---
+++ desafio-ponto-web/src/main/java/com/desafio/ponto/web/constants/DesafioPontoWebPortletKeys.java
@@ -0,0 +1,10 @@
+package com.desafio.ponto.web.constants;
+
+/**
+ * @author peterson.v.de.morais
+ */
+public class DesafioPontoWebPortletKeys {
+
+	public static final String DesafioPontoWeb = "desafiopontoweb";
+
+}
